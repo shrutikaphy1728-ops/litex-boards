e21aa0894352b6413f101158c347bca048578229
Shrutika Mohalkar <shrutikaphy1728@gmail.com>
2026-05-30 13:47:44 +0530

    Add platform and target files for Vaaman board

---
+++ litex_boards/platforms/vicara_vaaman.py
@@ -0,0 +1,85 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Master Platform Constraint File for the Vicara Vaaman Board (Rev 0.3)
+# Base Pin Mappings compiled from official Vicharak Hardware Documentation.
+
+from litex.build.generic_platform import *
+from litex.build.efinix import EfinixPlatform
+
+# I/O Pins -----------------------------------------------------------------------------------------
+
+_io = [
+    # --- Onboard FPGA Dedicated Clock Oscillators ---
+    ("clk25",     0, Pins("F10"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("clk50",     0, Pins("U8"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("clk30",     0, Pins("L14"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("clk20",     0, Pins("D9"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("clk74_25",  0, Pins("C9"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("clk10",     0, Pins("R8"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+
+    # --- Default Debug Serial Port ---
+    ("serial", 0,
+        Subsignal("tx", Pins("A10")), 
+        Subsignal("rx", Pins("B10")), 
+        IOStandard("3.3_V_LVTTL_/_LVCMOS")
+    ),
+
+    # --- Onboard FPGA Hardware Status/User LEDs ---
+    ("user_led", 0, Pins("A12"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("user_led", 1, Pins("B12"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("user_led", 2, Pins("A11"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+    ("user_led", 3, Pins("B11"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), 
+
+    # --- Complete FPGA 40-Pin GPIO Header Array ---
+    ("fpga_header_gpio", 0,  Pins("H13"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 07 
+    ("fpga_header_gpio", 1,  Pins("G13"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 08 
+    
+    # -----------------------------------------------------------------
+    # DEDICATED SERIAL PROTOCOLS (Replacing generic GPIOs 2 through 7)
+    # -----------------------------------------------------------------
+    ("spi", 0,
+        Subsignal("clk",  Pins("G5")),  # Was G05
+        Subsignal("mosi", Pins("H14")), 
+        Subsignal("miso", Pins("G6")),  # Was G06
+        Subsignal("cs_n", Pins("H6")),  # Was H06
+        IOStandard("3.3_V_LVTTL_/_LVCMOS")
+    ),
+    
+    ("i2c", 0,
+        Subsignal("scl", Pins("K6")),   # Was K06
+        Subsignal("sda", Pins("K15")),  
+        IOStandard("3.3_V_LVTTL_/_LVCMOS")
+    ),
+    # -----------------------------------------------------------------
+
+    #("fpga_header_gpio", 8,  Pins("A9"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Was A09
+    # Add this right above or below your existing "spi" block
+    ("winbond_cs", 0, Pins("A9"), IOStandard("3.3_V_LVTTL_/_LVCMOS")),
+    
+    ("fpga_header_gpio", 9,  Pins("B9"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Was B09 
+    ("fpga_header_gpio", 10, Pins("L16"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 25 
+    ("fpga_header_gpio", 11, Pins("E9"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Was E09 
+    ("fpga_header_gpio", 12, Pins("T8"),  IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Was T08
+    ("fpga_header_gpio", 13, Pins("L15"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 31 
+    ("fpga_header_gpio", 14, Pins("L17"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 32 
+    ("fpga_header_gpio", 15, Pins("L18"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 33 
+    ("fpga_header_gpio", 16, Pins("K17"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 34 
+    ("fpga_header_gpio", 17, Pins("M18"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 35 
+    ("fpga_header_gpio", 18, Pins("K16"), IOStandard("3.3_V_LVTTL_/_LVCMOS")), # Header Pin 36 
+]
+
+# Platform Configuration ---------------------------------------------------------------------------
+
+class Platform(EfinixPlatform):
+    default_clk_name   = "clk10"
+    default_clk_period = 1e9 / 10e6 # 40ns constraint evaluation baseline
+
+    def __init__(self):
+        EfinixPlatform.__init__(
+            self,
+            device      = "T120F324C3", 
+            io          = _io,
+            toolchain   = "efinity"
+        )
+        self.iobank_info = []
---
+++ litex_boards/targets/vicara_vaaman.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Target configuration for the Vicara Vaaman Board
+# Copyright (c) 2026 Shrutika Mohalkar
+# SPDX-License-Identifier: BSD-2-Clause
+
+from migen import *
+from litex_boards.platforms import vicara_vaaman
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=25e6, **kwargs):
+        # Load the master Vaaman platform pins we just made
+        platform = vicara_vaaman.Platform()
+
+        # Automatically wire up the core SoC structures (CPU, RAM, ROM, UART)
+        SoCCore.__init__(self, platform, sys_clk_freq,
+            ident          = "LiteX SoC on Vicara Vaaman",
+            ident_version  = True,
+            **kwargs
+        )
+
+        # Wire up the default LED chaser using the platform's user_led array
+        self.submodules.leds = LedChaser(
+            pads         = platform.request_all("user_led"),
+            sys_clk_freq = sys_clk_freq
+        )
+
+def main():
+    from litex.soc.integration.soc import litex_soc_args, litex_soc_argdict
+    from litex.build.parser import LiteXArgumentParser
+    
+    parser = LiteXArgumentParser(description="LiteX SoC on Vicara Vaaman")
+    target_group = parser.add_argument_group(title="Target options")
+    target_group.add_argument("--build", action="store_true", help="Build bitstream")
+    
+    builder_args(parser)
+    soc_core_args(parser)
+    args = parser.parse_args()
+
+    soc = BaseSoC(**soc_core_argdict(args))
+    builder = Builder(soc, **builder_argdict(args))
+    if args.build:
+        builder.build(run=True)
+
+if __name__ == "__main__":
+    main()
